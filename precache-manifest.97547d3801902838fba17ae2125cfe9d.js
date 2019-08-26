3c567b4e24c2e92a90df9f1eed9463db94becf33
Ирина <irbris@mail.ru>
2019-08-26 11:59:40 +0300

    Updates

---
+++ precache-manifest.97547d3801902838fba17ae2125cfe9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12fc0d206ac7a502f6d7a497cc052e9b",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "4e56754bdf47ca2dcd90",
+    "url": "/todo/static/css/main.69609c76.chunk.css"
+  },
+  {
+    "revision": "482968802acc2e1cbf7b",
+    "url": "/todo/static/js/2.0b13ce76.chunk.js"
+  },
+  {
+    "revision": "4e56754bdf47ca2dcd90",
+    "url": "/todo/static/js/main.d89a4466.chunk.js"
+  },
+  {
+    "revision": "b58d1d31989374fa9307",
+    "url": "/todo/static/js/runtime~main.3db048b5.js"
+  }
+]);
